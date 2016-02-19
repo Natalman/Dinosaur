4ca61b4b42c323bb23169cabea768bdafb93f79a
Natalman <sperobb@outlook.com>
2016-02-18 18:12:06 -0800

    Add a printout

--- src/com/Natalman/Dinosaursupervisor.java
+++ src/com/Natalman/Dinosaursupervisor.java
@@ -4,6 +4,7 @@ public class Dinosaursupervisor {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Dino prog");
         Dinosaur t_rex = new Dinosaur();
         Dinosaur velociraptor = new Dinosaur();
 
