fbf4cdab639f1c5f2d25eefc41d542081c149db0
Natalman <sperobb@outlook.com>
2016-02-18 18:07:03 -0800

    Creatd a dino Dinosur and called the roar method

---
+++ src/com/Natalman/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.Natalman;
+
+/**
+ * Created by Natalman on 2/18/2016.
+ */
+public class Dinosaur {
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ src/com/Natalman/Dinosaursupervisor.java
@@ -0,0 +1,13 @@
+package com.Natalman;
+
+public class Dinosaursupervisor {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+    }
+}
